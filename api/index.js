cb4dd825806e49545a0aadb85677c0e85d30588d
rlopez <rlopezaldana@gmail.com>
2021-06-04 15:28:08 -0500

    config response

--- api/index.js
+++ api/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 var ip = require("ip");
 
 router.get("/", (req, res, next) => {
-  res.send("service online : " + ip.address());
+  res.status(200).json({
+    service: "online",
+    ip: ip.address(),
+  });
 });
 
 module.exports = router;
